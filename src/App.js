390be987babb99271a4cf98bd34f861af6445468
Luu The Vinh <luuthevinh1988@gmail.com>
2023-04-09 10:40:50 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Dashboard, Login, PrivateRoute, AuthWrapper, Error} from './pages';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Route path='/'>
+        <Dashboard></Dashboard>
+      </Route>
+      <Route>
+        <Login></Login>
+      </Route>
+
+      <Error />
+    </Router>
+  );
+}
+
+export default App;
